1a31df4c7d445dff01a36bf7085b9cedbf4f6fe5
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2023-05-24 14:49:34 +0300

    Refactored and added new methods and error processing

--- main.rb
+++ main.rb
@@ -1,53 +1,43 @@
 # frozen_string_literal: true
 
-require_relative 'lib/railway_station'
-require_relative 'lib/train'
-require_relative 'lib/cargo_train'
-require_relative 'lib/passenger_train'
-require_relative 'lib/wagon'
-require_relative 'lib/cargo_wagon'
-require_relative 'lib/passenger_wagon'
 require_relative 'lib/route'
+require_relative 'lib/instance_counter'
+require_relative 'lib/train'
+require_relative 'lib/wagon'
+require_relative 'lib/cargo_train'
+require_relative 'lib/cargo_wagon'
 require_relative 'lib/company_name'
+require_relative 'lib/main_menu'
+require_relative 'lib/passenger_train'
+require_relative 'lib/passenger_wagon'
+require_relative 'lib/railway_station'
+require_relative 'lib/helpers/list_helpers'
+require_relative 'lib/helpers/search_helpers'
+require_relative 'lib/helpers/select_helpers'
 
 # User interface class
 class Main
+  include MainMenu
+  include ListHelpers
+  include SearchHelpers
+  include SelectHelpers
+
   attr_reader :stations
 
   def initialize
     @stations = []
     @trains = []
+    @routes = []
   end
-
-  MENU = "Выберите действие, которое хотите осуществить (введите число):\n" \
-      "1. Создать новую станцию\n" \
-      "2. Создать новый поезд\n" \
-      "3. Добавить вагоны к поезду\n" \
-      "4. Отцепить вагоны от поезда\n" \
-      "5. Переместить поезд на станцию\n" \
-      "6. Просмотреть список станций и список поездов на станции\n" \
-      '7. Завершить программу'
-
-  ACTIONS = {
-    1 => :add_new_railway_station,
-    2 => :create_new_train,
-    3 => :add_wagons,
-    4 => :pop_wagons,
-    5 => :set_train_to_station,
-    6 => :stations_list,
-    7 => :break_program
-  }.freeze
-
-  TYPES = {
-    1 => 'cargo',
-    2 => 'passenger'
-  }.freeze
 
   def start
     loop do
-      puts MENU
+      input = user_input(MENU).to_i
 
-      input = gets.chomp.to_i
+      if input.to_s !~ /^([0-9]|1[0-2])$/
+        puts "Ошибка: Введён неверный номер действия\n "
+        next
+      end
 
       send ACTIONS[input]
     end
@@ -55,57 +45,90 @@ class Main
 
   private
 
-  def add_new_railway_station
-    system('clear')
-    puts 'Введите имя станции:'
-    name = gets.chomp.to_s
-
-    @stations << RailwayStation.new(name)
-    system('clear')
+  def user_input(message)
+    puts message
+    gets.chomp
   end
 
-  def create_new_train
-    system('clear')
+  def create_railway_station
+    name = user_input('Введите имя станции (буквы, цифры, пробел):').to_s
 
-    puts 'Введите номер поезда (3 цифры):'
+    @stations << RailwayStation.new(name)
+  rescue StandardError => e
+    puts e.message
+    retry
+  end
 
-    number = gets.chomp.to_s
-
-    puts 'Выберите тип поезда (1. Грузовой, 2. Пассажирский):'
-
-    type = gets.chomp.to_i
+  def create_train
+    number = user_input('Введите номер поезда:').to_s
+    type = user_input('Выберите тип поезда (1. Грузовой, 2. Пассажирский):').to_i
 
     class_name = "#{TYPES[type].capitalize}Train"
-
     @trains << Object.const_get(class_name).new(number)
+  rescue StandardError => e
+    puts e.message
+    retry
+  end
+
+  def create_route
+    return puts 'Список станций пустой' if stations.empty?
+
+    name = user_input("Введите название маршрута:\n").to_s
+    stations_list
+    first_station = search_station(user_input("Введите название начальной станции:\n").to_s)
+    final_station = search_station(user_input("Введите название конечной станции:\n").to_s)
+
+    @routes << Route.new(name, first_station, final_station)
+  rescue StandardError => e
+    puts e.message
+    retry
+  end
+
+  def add_station_to_route
+    route = select_route
+    station = select_station
+    raise StandardError, 'Такая станция уже есть в маршруте' if route.route.include? station
+
+    route.add_station(station)
+  rescue StandardError => e
+    puts e.message
+    retry
+  end
+
+  def delete_station_from_route
+    route = select_route
+    station = select_station
+
+    route.remove_station(station)
+  end
+
+  def set_route_to_train
+    route = select_route
+    train = select_train
+
+    train.new_route(route)
+  rescue StandardError => e
+    puts e.message
+    retry
   end
 
   def add_wagons
-    system('clear')
-
-    puts 'Введите номер поезда, к которому нужно прицепить вагоны (3 цифры):'
-    puts @trains.inspect
-
-    number = gets.chomp.to_s
+    number = user_input("Введите номер поезда, к которому нужно прицепить вагоны (3 цифры):\n#{@trains.inspect}").to_s
 
     train = search_train(number)
 
-    type = train.type
-
-    class_name = "#{type.capitalize}Wagon"
+    class_name = "#{train.type.capitalize}Wagon"
 
     wagon = Object.const_get(class_name).new
 
     train.attach_wagon(wagon)
+  rescue StandardError => e
+    puts e.message
+    retry
   end
 
   def pop_wagons
-    system('clear')
-
-    puts 'Введите номер поезда, от которого нужно отцепить вагоны (3 цифры):'
-    puts @trains.inspect
-
-    number = gets.chomp.to_s
+    number = user_input("Введите номер поезда, от которого нужно отцепить вагоны (3 цифры):\n#{@trains.inspect}").to_s
 
     train = search_train(number)
 
@@ -113,41 +136,14 @@ class Main
   end
 
   def set_train_to_station
-    system('clear')
-    puts 'Введите номер поезда, который вы хотите переместить на станцию (3 цифры):'
-    puts @trains.inspect
+    number = user_input("Введите номер поезда, который вы хотите переместить:\n#{@trains.inspect}").to_s
 
-    number = gets.chomp.to_s
-
-    puts 'Выберите название станции, на которую выхотите переместить поезд:'
-    puts @stations.inspect
-
-    station_name = gets.chomp.to_s
+    station_name = user_input("Выберите название станции, куда выхотите переместить поезд:\n#{@stations.inspect}").to_s
 
     train = search_train(number)
     station = search_station(station_name)
 
     station.add_train(train)
-  end
-
-  def stations_list
-    system('clear')
-    stations.each do |station|
-      puts station.inspect
-      cargo_trains = station.trains_by_type('cargo')
-      passenger_trains = station.trains_by_type('passenger')
-
-      puts "Грузовые поезда: #{cargo_trains.inspect}" if cargo_trains.any?
-      puts "Пассажирские поезда: #{passenger_trains.inspect}" if passenger_trains.any?
-    end
-  end
-
-  def search_train(number)
-    @trains.find { |train| train.number == number }
-  end
-
-  def search_station(station_name)
-    @stations.find { |station| station.name == station_name }
   end
 
   def break_program
